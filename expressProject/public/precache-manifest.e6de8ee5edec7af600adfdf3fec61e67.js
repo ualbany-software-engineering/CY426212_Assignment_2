a84c323ee0487ae666ca0ff8377e1575b0ae882b
manikanta7 <manikantayarramaneni.1998@gmail.com>
2022-10-10 01:07:14 -0400

    Initial commit

---
+++ expressProject/public/precache-manifest.e6de8ee5edec7af600adfdf3fec61e67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b34ad4fd67f5351b05da",
+    "url": "/static/css/main.5f9ba6d6.chunk.css"
+  },
+  {
+    "revision": "b34ad4fd67f5351b05da",
+    "url": "/static/js/main.b34ad4fd.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "c634e1b2f4de3e0fec83",
+    "url": "/static/js/2.c634e1b2.chunk.js"
+  },
+  {
+    "revision": "7af4db86e54e523836d2",
+    "url": "/static/js/runtime~main.7af4db86.js"
+  },
+  {
+    "revision": "555bb565c8d633e7bb628c95e905aa09",
+    "url": "/static/media/image.555bb565.jpeg"
+  },
+  {
+    "revision": "922213d1eda0e8ec57b09cd35d91695e",
+    "url": "/index.html"
+  }
+];
